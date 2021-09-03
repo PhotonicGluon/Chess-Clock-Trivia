b444f29b5a292568ad54dd1d8fb0c76ad111b20d
Ryan-Kan <25820201+Ryan-Kan@users.noreply.github.com>
2021-09-03 15:56:09 +0800

    Ratelimit Pages
    - Add ratelimit codes to the pages
    - Added error pages

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 main.py
 
 Created on 2021-08-12
-Updated on 2021-09-01
+Updated on 2021-09-03
 
 Copyright © Ryan Kan
 
@@ -18,6 +18,9 @@ from random import choices, Random
 
 import redis
 from flask import Flask, render_template, send_file, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from werkzeug.exceptions import HTTPException
 
 # CONSTANTS
 CREDITS_FILE = "data/credits.md"
@@ -46,8 +49,9 @@ with open(RULES_FILE, "r") as f:
 with open(CREDITS_FILE, "r") as f:
     creditsMD = f.read()  # The text in the `RULES_FILE` is markdown text
 
-# Set up the app instance
+# Set up the app instance with rate limiting capabilities
 app = Flask(__name__)
+limiter = Limiter(app, key_func=get_remote_address)
 
 # Connect to the redis sever and check if successfully connected
 redisDB = redis.from_url(os.getenv("REDIS_URL"))
@@ -75,27 +79,32 @@ def get_questions_from_session(session_id):
 
 # VIEWABLE PAGES
 @app.route("/")
+@limiter.limit("3/second")
 def main_page():
     return render_template("main_page.html", num_questions=numQuestions)
 
 
 @app.route("/questioner")
+@limiter.limit("3/second")
 def questioner():
     return render_template("questioner.html", num_questions=numQuestions)
 
 
 @app.route("/rules")
+@limiter.limit("3/second")
 def rules():
     return render_template("rules.html", rules=rulesMD)
 
 
 @app.route("/credits")
+@limiter.limit("3/second")
 def credits_page():
     return render_template("credits.html", credits=creditsMD)
 
 
 # CODE-ONLY PAGES
 @app.route("/code-only/heartbeat", methods=["POST"])
+@limiter.limit("1/minute")
 def heartbeat():
     # Get the data from the submitted form
     data = request.form
@@ -114,11 +123,13 @@ def heartbeat():
 
 
 @app.route("/code-only/generate-session-id", methods=["POST"])
+@limiter.limit("3/second")
 def generate_session_id():
     return " ".join(choices(seedWords, k=SEED_LENGTH))
 
 
 @app.route("/code-only/get-questions", methods=["POST"])
+@limiter.limit("1/10second")
 def get_questions():
     # Get the data from the submitted form
     data = request.form
@@ -132,6 +143,7 @@ def get_questions():
 
 
 @app.route("/code-only/set-up-session", methods=["POST"])
+@limiter.limit("1/10second")
 def set_up_session():
     # Get the data from the submitted form
     data = request.form
@@ -164,6 +176,7 @@ def set_up_session():
 
 
 @app.route("/code-only/update-session", methods=["POST"])
+@limiter.limit("1/5second")
 def update_session():
     # Get the data from the submitted form
     data = request.form
@@ -197,8 +210,25 @@ def update_session():
         return f"Invalid question number '{data['question_num']}'."
 
 
+# ERROR PAGES
+@app.errorhandler(HTTPException)
+def ratelimit_handler(e):
+    # Get the code, name and  description from the exception
+    code = e.code
+    name = e.name
+    desc = e.description
+
+    # Handle specific errors
+    if e.code == 429:  # Too many requests
+        desc = f"You have made too many requests (maximum permitted: {desc}). Slow down your requests and try again."
+
+    # Return the error page
+    return render_template("error_page.html", error_code=code, error_name=name, error_desc=desc)
+
+
 # MISCELLANEOUS PAGES
 @app.route("/favicon.ico")
+@limiter.limit("3/second")
 def favicon():
     return send_file("static/resources/img/favicon.ico")  # Access local `favicon.ico` file
 
